4ff109f0b85e8a2583aed19cb9d47494a1b64c41
AhmedM16 <ahmedmarwan105@gmail.com>
2022-07-26 02:55:37 +0300

    Cards & Navbar Adeed

--- week01/src/components/main.js
+++ week01/src/components/main.js
@@ -4,8 +4,9 @@ import HornedBeast from "./HornedBeast";
 class Main extends React.Component {
     render(){
         return <>
-        <HornedBeast title='Horned Beast 1' imageUrl='https://images.unsplash.com/photo-1614029362896-abd39216179d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG9ybmVkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' description='This is a Description' />
-        <HornedBeast title='Horned Beast 2' imageUrl='https://images.unsplash.com/photo-1638117919075-f9723b95b81c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80' description='This is a Description' />
+        <HornedBeast title='Dear' imageUrl='https://images.unsplash.com/photo-1638117919075-f9723b95b81c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80' description='Number of Votes' />
+        <HornedBeast title='Rhino' imageUrl='https://images.unsplash.com/photo-1614029362896-abd39216179d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG9ybmVkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' description='Number of Votes' />
+        <HornedBeast title='Dear' imageUrl='https://images.unsplash.com/photo-1638117919075-f9723b95b81c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80' description='Number of Votes' />
         </>
     }
 }
--- week01/src/components/navbar.js
+++ week01/src/components/navbar.js
@@ -1,13 +1,13 @@
 import Container from 'react-bootstrap/Container';
+import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Horned Beast Project </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
